620f23801ca2baf2a93e1ec2a2e66d7277dd1339
Seung-Chan <dalek@hallym.ac.kr>
2019-01-19 01:05:01 +0900

    gitignore

--- 04_Kalman_in_c/02_KalmanCVModel/KalmanCVModel/KalmanCVModel/KalmanCVModel.cpp
+++ 04_Kalman_in_c/02_KalmanCVModel/KalmanCVModel/KalmanCVModel/KalmanCVModel.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
-
+#include "kalman.h"
 
 int main()
 {
-    return 0;
+	
+	int sz = 10;
+
+	for (int i = 0; i < sz; i++)
+	{
+		float t0 = data_t[i];
+		float px = data_px[i];
+		float py = data_py[i];
+
+		printf("%d\t%f\t%f\t%f\n", i, t0, px, py);
+
+	}
+	return 0;
 }
 
