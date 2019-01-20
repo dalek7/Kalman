9e81edb648a50d2695cb935b01210a896ef27665
Seung@5142 <dalek@kaist.ac.kr>
2019-01-20 11:00:52 +0900

    validation

---
+++ 04_Kalman_in_c/02_KalmanCVModel/KalmanCVModel/KalmanCVModel/testmat.m
@@ -0,0 +1,18 @@
+clc; clear all; close all;
+
+A =[ 2 3; 1 4];
+B = inv(A);
+disp(A);
+disp(B);
+
+
+A =[ 3 3; 5 4];
+B = inv(A);
+disp(A);
+disp(B);
+
+
+A =[ 7 -1.5; 5 2];
+B = inv(A);
+disp(A);
+disp(B);
